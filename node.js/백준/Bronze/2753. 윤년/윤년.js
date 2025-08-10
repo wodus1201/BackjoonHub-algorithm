15d6041ea2d9c40349e9ed83091dc51c2fe6a0a7
Jason Park (박재연) <lienemapille@gmail.com>
2025-08-10 17:15:31 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 9352 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "run/input.txt";
+const input = fs.readFileSync(filePath).toString().trim();
+
+const year = Number(input);
+
+if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+  console.log(1);
+} else {
+  console.log(0);
+}
