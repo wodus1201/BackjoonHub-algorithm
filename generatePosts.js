b52c2e8139e050291807408c0828ad58822a4923
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-08-11 20:42:42 +0900

    chore: md 파일 생성 시 학습날짜로 이름적용

--- generatePosts.js
+++ generatePosts.js
@@ -13,9 +13,6 @@ function createMarkdownForFolder(folderName) {
   // 폴더명 내 넓은 공백 및 특수 공백을 일반 하이픈으로 교체
   const sanitizedFolderName = folderName.replace(/[\s\u2000-\u200A\u202F\u205F\u3000]+/g, "-");
   const folderPath = path.join(baseDir, folderName);
-  const postDate = formatDate(new Date());
-  const postFileName = `${postDate}-${sanitizedFolderName}.md`;
-  const postFilePath = path.join(postsDir, postFileName);
 
   console.log(`Looking for folder: ${folderPath}`);
 
@@ -29,7 +26,27 @@ function createMarkdownForFolder(folderName) {
   let readmeContent = "";
   if (fs.existsSync(readmePath)) {
     readmeContent = fs.readFileSync(readmePath, "utf-8");
+    // Replace all instances of http:// with https://
+    readmeContent = readmeContent.replace(/http:\/\//g, "https://");
   }
+
+  // 제출 일자에서 날짜 추출 (README 우선, 시간 무시)
+  let postDate = formatDate(new Date());
+  const lines = readmeContent.split(/\r?\n/);
+  const dateRegex = /(\d{4})년\s*(\d{1,2})월\s*(\d{1,2})일/;
+  for (const line of lines) {
+    if (line.includes("202")) {
+      const dateMatch = line.match(dateRegex);
+      if (dateMatch) {
+        let [, year, month, day] = dateMatch;
+        postDate = `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+        break;
+      }
+    }
+  }
+
+  const postFileName = `${postDate}-${sanitizedFolderName}.md`;
+  const postFilePath = path.join(postsDir, postFileName);
 
   // js 파일 목록과 내용 읽기
   const files = fs.readdirSync(folderPath);
